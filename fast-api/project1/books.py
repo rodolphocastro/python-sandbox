1565edea64bc5ecd967c5d9e7ab35888b1f0ce3d
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2025-01-11 20:51:31 -0300

    chore: log whenever there's a hit on the endpoint

--- fast-api/project1/books.py
+++ fast-api/project1/books.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+import logging
 
-print("starting the api")
+logger = logging.getLogger("uvicorn") # use the FastAPI (uvicorn) logger
+
+logger.info("starting the FastAPI server...")
 app = FastAPI() # instantiate the FastAPI class, which we'll be attaching methods to
 
 # creating a method that'll listen to GET requests on the / route of our server
 @app.get("/")
 def say_hello():
+    logger.info("greeting an user")
     return {"message": "Hello, World!"}
