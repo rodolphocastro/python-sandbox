2b4e22038157be59712e45bb0fef8c1d1b86f73a
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2025-01-11 21:59:31 -0300

    feat: GET books

--- fast-api/project1/books.py
+++ fast-api/project1/books.py
@@ -6,8 +6,16 @@ logger = logging.getLogger("uvicorn") # use the FastAPI (uvicorn) logger
 logger.info("starting the FastAPI server...")
 app = FastAPI() # instantiate the FastAPI class, which we'll be attaching methods to
 
+logger.info("creating a few books")
+BOOKS = [
+    {"title": "Harry Potter", "author": "J.K. Rowling", "category": "fantasy"},
+    {"title": "The Lord of the Rings", "author": "J.R.R. Tolkien", "category": "fantasy"},
+    {"title": "To Kill a Mockingbird", "author": "Harper Lee", "category": "drama"},
+    {"title": "The Alchemist", "author": "Paulo Coelho", "category": "adventure"}
+]
+
 # creating a method that'll listen to GET requests on the / route of our server
-@app.get("/")
-def say_hello():
-    logger.info("greeting an user")
-    return {"message": "Hello, World!"}
+@app.get("/books")
+def all_books():
+    logger.info("returning all the books")
+    return BOOKS
