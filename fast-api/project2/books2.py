7addb01392c6532d43fe4d1da4ca9edbdf3ac943
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2025-01-23 11:52:44 -0300

    feat: return actual book objects

--- fast-api/project2/books2.py
+++ fast-api/project2/books2.py
@@ -15,7 +15,14 @@ class Book:
 
 app = FastAPI()
 
-BOOKS = []
+BOOKS = [
+    Book(1, "Computer Science Pro", "Doe, John", "A very nice book", 5),
+    Book(2, "Be FAST with FastAPI", "Doe, John", "A great book", 5),
+    Book(3, "Master endpoints", "Doe, John", "An awesome book", 5),
+    Book(4, "HP1", "Author 1", "Book description", 2),
+    Book(5, "HP2", "Author 2", "Book description", 3),
+    Book(6, "HP3", "Author 3", "Book description", 1),
+]
 
 @app.get("/books")
 async def get_all_books():
