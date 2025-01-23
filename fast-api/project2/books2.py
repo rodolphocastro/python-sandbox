a4dec36e9bc4bb2d3ea82ff44130d7d2095c8917
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2025-01-23 11:46:51 -0300

    feat: create a Book class

--- fast-api/project2/books2.py
+++ fast-api/project2/books2.py
@@ -1,6 +1,18 @@
 from typing import Self
 from fastapi import FastAPI
 
+class Book:
+    """
+    represents a physical book in our API.
+    """
+
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int) -> Self:
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
 app = FastAPI()
 
 BOOKS = []
