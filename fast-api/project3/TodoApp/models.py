492707d05264427739b6f6779ddcd3b71565c946
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2025-01-24 16:13:56 -0300

    feat: describe a TODO model

---
+++ fast-api/project3/TodoApp/models.py
@@ -0,0 +1,33 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Todos(Base):
+    """
+    a model representing a To-Do object.
+    """
+    __tablename__ = "todos"
+
+    """
+    primary key column for our To-Do
+    """
+    id = Column(Integer, primary_key=True, index=True)
+
+    """
+    the name of our to-do
+    """
+    title = Column(String)
+
+    """
+    a brief description of our to-do
+    """
+    description = Column(String)
+
+    """
+    the priority of our to-do
+    """
+    priority = Column(Integer)
+
+    """
+    status of our to-do, if it is completed or not. Defaults to false (not completed)
+    """
+    complete = Column(Boolean, default=False)
