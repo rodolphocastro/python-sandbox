51cc1e24ee8ddf3f94db60b7a8e75ad5fd5e8a61
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2025-01-30 22:19:05 -0300

    feat: get all TODOs

--- fast-api/project3/TodoApp/main.py
+++ fast-api/project3/TodoApp/main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import Depends, FastAPI
+from database import engine, local_session
+from models import Todos
 import models
-from database import engine
 
 app = FastAPI()
 """
 our fastapi application
 """
 
+# only executed if the db doesn't exist!
 models.database.metadata.create_all(bind=engine)
+
+def get_db():
+    """
+    gets a working local session of our database.
+    it automatically closes the database connection once done.
+    """
+    db = local_session()
+    try:
+        # yield is a type of return with extra logic to handle doing something after it is finished.ß
+        yield db
+    finally:
+        db.close()
+
+@app.get("/todos")
+async def get_all_todos(db: Annotated[Session, Depends(get_db)]):
+    """
+    gets all the TODOs currently in the system.
+    """
+    return db.query(Todos).all()
